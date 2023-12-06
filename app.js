349fcde9ace612d8c27e662760b672abd5460c85
Hamza <hamza05259@gmail.com>
2023-12-06 15:20:56 +0300

    Fotoğrafların ekranda gösterilmesi

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ runEventListeners();
 
 function runEventListeners() {
     form.addEventListener("submit", search);
+    clearBtn.addEventListener("click", clear);
 }
 
 function search(e) {
-    
     const value = searchInput.value.trim();
     console.log(value);
     fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
@@ -23,13 +23,37 @@ function search(e) {
             Authorization: "Client-ID 9EhJutdYidlkVQ84o8mugCnhyLrNILOLinDIL_nL1L8"
         }
     })
-    .then((response) => response.json())
-    .then((data) => {
-        data.results.forEach(image => {
-            console.log(image.urls.small)
-        });
-    })
-    .catch((err) => console.log(err))
+        .then((response) => response.json())
+        .then((data) => {
+            data.results.forEach(image => {
+                //console.log(image.urls.small)
+                addImageToUI(image.urls.small);
+            });
+        })
+        .catch((err) => console.log(err))
 
     e.preventDefault();
+}
+
+function addImageToUI(url) {
+    const col = document.createElement("div");
+    col.className = "col-md-4"
+
+    const div = document.createElement("div");
+    div.className = "card m-3";
+
+    const image = document.createElement("img");
+    image.src = url;
+    image.width = "400";
+    image.height = "300";
+    image.className = "card-img thumbnail";
+
+    div.appendChild(image);
+    col.appendChild(div);
+    imageListWrapper.appendChild(col);
+}
+
+function clear() {
+    searchInput.value = "";
+    imageListWrapper.innerHTML = "";
 }
